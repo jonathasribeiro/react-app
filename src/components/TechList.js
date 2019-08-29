1f11d798272d9b7402fb846b73cea9fdcaabce9a
Jonathas <jonathasribeiroreal@gmail.com>
2019-08-29 16:40:14 -0300

    Aula 7 -Modulo 1

---
+++ src/components/TechList.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Techlist extends Component {
+  state = {
+    newTech: "",
+    techs: ["Node.js", "ReactJs", "React Native"]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default Techlist;
